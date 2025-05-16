f67dbb0be6a6d6d564fae0d6bf1b1eedeb9c47f9
Remy-Amor <105914423@student.swin.edu.au>
2025-05-16 21:04:15 +1000

    added styling

--- project2/manager_signup.php
+++ project2/manager_signup.php
@@ -9,6 +9,16 @@
 <head>
      <meta charset="UTF-8">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <meta name="author" content="Remy Amor, William Anthony, Amanuial Ashagrie">
+     <meta name="description" content="Homepage of Dream Team IT Solutions">
+     <meta name="keywords" content="homepage, IT, software, software development, network, network administrator, jobs">
+     <!-- link to styles.css -->
+     <link rel="stylesheet" href="project1/styles/styles.css">
+     <link rel="stylesheet" href="styles/styles.css">
+     <!-- <link rel="stylesheet" href="styles/aman_styles.css"> -->
+
+     <!-- link to responsive css -->
+     <link rel="stylesheet" href="styles/responsive.css">
      <title>Manager Signup</title>
 </head>
 <body>
