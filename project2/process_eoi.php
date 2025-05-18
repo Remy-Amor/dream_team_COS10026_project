2951ef1e691fd158413bedb3d30ca58fc6b1aa44
amangitfr <105871858@student.swin.edu.au>
2025-05-18 23:20:11 +1000

    making eoi_num var

--- project2/process_eoi.php
+++ project2/process_eoi.php
@@ -25,7 +25,7 @@
         $sql = "INSERT INTO eoi_tb (job_ref_no, first_name, last_name, street_address, suburb_town, state, post_code, email, phone_number, network_admin_skills, software_developer_skills, other_skills) VALUES ('$job_ref_no', '$first_name', '$last_name', '$street_address', '$suburb_town', '$state', '$post_code', '$email', '$phone_number', '$network_admin_skills', '$software_developer_skills', '$other_skills')";
     }
 
-    
+    $eoi_number = "SELECT EOInumber FROM eoi_tb WHERE firs_name LIKE '%$first_name%'";
 ?>
 
 <!DOCTYPE html>
