d5b7a26f6cfa5e07e41eb3ed9275144393164866
Remy-Amor <105914423@student.swin.edu.au>
2025-05-20 15:24:29 +1000

    started making query option manage.php

--- project2/manage.php
+++ project2/manage.php
@@ -72,6 +72,18 @@
                     echo "<h3>No Records Found</h3>";
                }
           ?>
+
+          <h3>Make a Query</h3>
+          <h4 class="query-header">Display based on Job Reference Number</h4>
+          <form action="manage_results.php" method="post">
+               <label for="job_ref_search">Job Reference:</label>
+               <select name="job_ref_search" id="job_ref_search" required>
+                    <option value="">Select a job reference number</option>
+                    <option value="SE41B">SE41B</option>
+                    <option value=" NA23X"> NA23X</option>
+               </select>
+               <input type="submit" value="submit">
+          </form>
           
      </main>
      <?php include("footer.inc"); ?>
