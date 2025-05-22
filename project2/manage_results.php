ff1f55b309f6b7ba854f2a5d2d164df2b263d86b
Remy-Amor <105914423@student.swin.edu.au>
2025-05-22 22:15:47 +1000

    finished manage.php basic requirements, next need to make all displaying of job reference numbers dynamic, and after that I can do enhancements

--- project2/manage_results.php
+++ project2/manage_results.php
@@ -38,46 +38,14 @@
                     $search_job_ref = sanitize_input($_POST["search_job_ref"]);
                     $sql = "SELECT * from eoi_tb WHERE job_ref_no = '$search_job_ref'";
                     $result = mysqli_query($conn, $sql);
-                    if(mysqli_num_rows($result)>0) {
-                         echo "<div class='table-wrapper'><table>";
-                         echo "<tr><th>EOI number</th><th>Job Reference</th><th>First Name</th><th>Last Name</th>
-                         <th>Street address</th><th>Suburb/town</th><th>State</th><th>Post Code</th>
-                         <th>email</th><th>Phone number</th><th>Network admin skills</th>
-                         <th>Software developer skills</th><th>Other skills</th><th>Change status</th><th>Change Status</th></tr>";
-                    while ($row = mysqli_fetch_assoc($result)) {
-                         $network_admin = $row['network_admin_skills'] ? 'true' : 'false';
-                         $software_development = $row['software_developer_skills'] ? 'true' : 'false';
-                         echo "<tr>
-                         <td>" . $row['EOInumber'] . "</td>"
-                         . "<td>" . $row['job_ref_no'] . "</td>"
-                         . "<td>" . $row['first_name'] . "</td>"
-                         . "<td>" . $row['last_name'] . "</td>"
-                         . "<td>" . $row['street_address'] . "</td>"
-                         . "<td>" . $row['suburb_town'] . "</td>"
-                         . "<td>" . $row['state'] . "</td>"
-                         . "<td>" . $row['postcode'] . "</td>
-                         <td>" . $row['email'] . "</td>"
-                         . "<td>" . $row['phone_number'] . "</td>"
-                         . "<td>" . $network_admin . "</td>"
-                         . "<td>" . $software_development . "</td>"
-                         . "<td>" . $row['other_skills'] . "</td>"
-                         . "<td>" . $row['eoi_status'] . "</td>"
-                         . "<td> <form action='change_eoi.php' method='post' novalidate='novalidate'>
-                              <select name='status' id='status' required>
-                                   <option value=''>Change Status</option>
-                                   <option value='New'>New</option>
-                                   <option value='Current'>Current</option>
-                                   <option value='Final'>Final</option>
-                              </select>
-                              <input type='hidden' name='EOInumber' value='" . $row['EOInumber'] . "'>
-                              <input type='submit' value='change'> 
-                         </form> </td>"
-                         . "</tr>";
-                    }
-                     echo "</table></div>";
-               } else {
-                    echo "<h3>No Records Found</h3>";
                }
+               // for deleting by job reference number
+               if(isset($_POST["delete_by_job_ref"])) {
+                    $delete_job_ref = sanitize_input($_POST["delete_job_ref"]);
+                    $sql = "DELETE FROM eoi_tb WHERE job_ref_no = '$delete_job_ref'";
+                    mysqli_query($conn, $sql);
+                    $sql = "SELECT * FROM eoi_tb";
+                    $result = mysqli_query($conn, $sql);
                }
 
                //  for searching by name
@@ -96,47 +64,49 @@
                          $sql .= " AND last_name = '$search_last_name'";
                      }
                     $result = mysqli_query($conn, $sql);
-                    if(mysqli_num_rows($result)>0) {
-                         echo "<div class='table-wrapper'><table>";
-                         echo "<tr><th>EOI number</th><th>Job Reference</th><th>First Name</th><th>Last Name</th>
-                         <th>Street address</th><th>Suburb/town</th><th>State</th><th>Post Code</th>
-                         <th>email</th><th>Phone number</th><th>Network admin skills</th>
-                         <th>Software developer skills</th><th>Other skills</th><th>Change status</th><th>Change Status</th></tr>";
-                    while ($row = mysqli_fetch_assoc($result)) {
-                         $network_admin = $row['network_admin_skills'] ? 'true' : 'false';
-                         $software_development = $row['software_developer_skills'] ? 'true' : 'false';
-                         echo "<tr>
-                         <td>" . $row['EOInumber'] . "</td>"
-                         . "<td>" . $row['job_ref_no'] . "</td>"
-                         . "<td>" . $row['first_name'] . "</td>"
-                         . "<td>" . $row['last_name'] . "</td>"
-                         . "<td>" . $row['street_address'] . "</td>"
-                         . "<td>" . $row['suburb_town'] . "</td>"
-                         . "<td>" . $row['state'] . "</td>"
-                         . "<td>" . $row['postcode'] . "</td>
-                         <td>" . $row['email'] . "</td>"
-                         . "<td>" . $row['phone_number'] . "</td>"
-                         . "<td>" . $network_admin . "</td>"
-                         . "<td>" . $software_development . "</td>"
-                         . "<td>" . $row['other_skills'] . "</td>"
-                         . "<td>" . $row['eoi_status'] . "</td>"
-                         . "<td> <form action='change_eoi.php' method='post' novalidate='novalidate'>
-                              <select name='status' id='status' required>
-                                   <option value=''>Change Status</option>
-                                   <option value='New'>New</option>
-                                   <option value='Current'>Current</option>
-                                   <option value='Final'>Final</option>
-                              </select>
-                              <input type='hidden' name='EOInumber' value='" . $row['EOInumber'] . "'>
-                              <input type='submit' value='change'> 
-                         </form> </td>"
-                         . "</tr>";
-                    }
-                     echo "</table></div>";
-               } else {
-                    echo "<h3>No Records Found</h3>";
                }
+
+               // output resulting table
+               if(mysqli_num_rows($result)>0) {
+                    echo "<div class='table-wrapper'><table>";
+                    echo "<tr><th>EOI number</th><th>Job Reference</th><th>First Name</th><th>Last Name</th>
+                    <th>Street address</th><th>Suburb/town</th><th>State</th><th>Post Code</th>
+                    <th>email</th><th>Phone number</th><th>Network admin skills</th>
+                    <th>Software developer skills</th><th>Other skills</th><th>Change status</th><th>Change Status</th></tr>";
+               while ($row = mysqli_fetch_assoc($result)) {
+                    $network_admin = $row['network_admin_skills'] ? 'true' : 'false';
+                    $software_development = $row['software_developer_skills'] ? 'true' : 'false';
+                    echo "<tr>
+                    <td>" . $row['EOInumber'] . "</td>"
+                    . "<td>" . $row['job_ref_no'] . "</td>"
+                    . "<td>" . $row['first_name'] . "</td>"
+                    . "<td>" . $row['last_name'] . "</td>"
+                    . "<td>" . $row['street_address'] . "</td>"
+                    . "<td>" . $row['suburb_town'] . "</td>"
+                    . "<td>" . $row['state'] . "</td>"
+                    . "<td>" . $row['postcode'] . "</td>
+                    <td>" . $row['email'] . "</td>"
+                    . "<td>" . $row['phone_number'] . "</td>"
+                    . "<td>" . $network_admin . "</td>"
+                    . "<td>" . $software_development . "</td>"
+                    . "<td>" . $row['other_skills'] . "</td>"
+                    . "<td>" . $row['eoi_status'] . "</td>"
+                    . "<td> <form action='change_eoi.php' method='post' novalidate='novalidate'>
+                         <select name='status' id='status' required>
+                              <option value=''>Change Status</option>
+                              <option value='New'>New</option>
+                              <option value='Current'>Current</option>
+                              <option value='Final'>Final</option>
+                         </select>
+                         <input type='hidden' name='EOInumber' value='" . $row['EOInumber'] . "'>
+                         <input type='submit' value='change'> 
+                    </form> </td>"
+                    . "</tr>";
                }
+                echo "</table></div>";
+          } else {
+               echo "<h3>No Records Found</h3>";
+          }
           }
           ?>
      </main>
